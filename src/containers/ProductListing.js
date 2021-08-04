e88909f5f7b3cfc5f455cc8dcaecc9093aa627b9
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-08-04 15:48:23 -0300

    Avoid function dependency to change on every render

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, {useEffect} from 'react'
+import React, {useEffect, useCallback} from 'react'
 import {useDispatch} from 'react-redux'
 import {setProducts} from 'redux/actions/productActions'
 import ProductComponent from './ProductComponent'
@@ -7,14 +7,14 @@ import ProductComponent from './ProductComponent'
 function ProductListing() {
   const dispatch = useDispatch()
 
-  const fetchProducts = async () => {
+  const fetchProducts = useCallback(async () => {
     const response = await axios.get('https://fakestoreapi.com/products')
     dispatch(setProducts(response.data))
-  }
+  }, [dispatch])
 
   useEffect(() => {
     fetchProducts()
-  }, [])
+  }, [fetchProducts])
 
   return (
     <div className="ui grid container">
