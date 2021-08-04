da9da8eb042fc823f41345ab445b31d067e59a7f
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-08-04 14:23:57 -0300

    Creating the routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css'
 import Header from 'containers/Header'
+import ProductListing from 'containers/ProductListing'
+import ProductDetail from 'containers/ProductDetail'
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" component={ProductListing} exact />
+          <Route path="/product/:productId" component={ProductDetail} exact />
+          <Route>Not found!</Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
